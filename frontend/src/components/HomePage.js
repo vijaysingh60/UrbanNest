bbb7929e74c08ff348b8e9de4018cb041fb031d2
vijaysingh60 <121249393+vijaysingh60@users.noreply.github.com>
2025-02-14 08:57:44 +0530

    Update HomePage.js
    mobile responsive

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -22,7 +22,7 @@ function HomePage() {
   return (
     <div className="homepage m-0 p-0 w-full  bg-white" >
         
-      <header className="header pt-24 w-full pl-10 bg-white">
+      <header className="header sm:pt-24 pt-10 pl-5 w-full sm:pl-10 bg-white">
         <div className='flex  justify-between'>
             <div>
                 <div className=" mb-10 pt-10">
@@ -33,27 +33,27 @@ function HomePage() {
                 {/* Search Bar */}
                 <HomeSearch isHome={true}/>
             </div>
-            <img alt='lodo' className='w-2/5' src={require('./images/img.webp')}></img>
+            <img alt='lodo' className='w-2/5 hidden sm:block' src={require('./images/img.webp')}></img>
         </div>
         
         
 
 
         {/* Buttons for Listing and Finding a Place */}
-        <div className="flex  gap-24 mb-6 w-full pl-10">
+        <div className="flex  gap-12 sm:gap-24 mb-6 pr-5 w-full">
             {/* List a place */}
-            <div className="w-2/5 relative cursor-pointer hover:shadow-xl h-48  overflow-hidden rounded-es-[5rem] rounded-se-[5rem] shadow-lg flex flex-col items-center justify-center " 
+            <div className="w-1/2 sm:2/5 relative cursor-pointer hover:shadow-xl h-48  overflow-hidden rounded-es-[5rem] rounded-se-[5rem] shadow-lg flex flex-col items-center justify-center " 
             ><img className='absolute w-full h-full z-0' src={require('./images/list.jpg')}></img>
-            <Link to={"/list/:"}><button className="flex items-center gap-2 relative z-10 bg-gray-800 text-white px-16 py-3 rounded-lg font-semibold">
+            <Link to={"/list/:"}><button className="flex items-center gap-2 relative z-5 bg-gray-800 text-white px-6 sm:px-16 py-3 rounded-lg font-semibold">
                     <BsPlusCircle size={20} />
                     List a place
                 </button></Link>
             </div>
 
             {/* Find a place */}
-            <div className="w-2/5 overflow-hidden relative cursor-pointer hover:shadow-xl h-48 bg-gray-200 rounded-lg shadow-lg flex rounded-ss-[5rem] rounded-ee-[5rem] flex-col items-center justify-center">
+            <div className="w-1/2 sm:2/5 overflow-hidden relative cursor-pointer hover:shadow-xl h-48 bg-gray-200 rounded-lg shadow-lg flex rounded-ss-[5rem] rounded-ee-[5rem] flex-col items-center justify-center">
             <img className='absolute w-full h-full z-0' src={require('./images/find.jpeg')}></img>
-                <Link to={"/for-rent/:"}><button className="flex items-center relative z-10 gap-2 bg-gray-800 text-white px-16 py-3 rounded-lg font-semibold">
+                <Link to={"/for-rent/:"}><button className="flex items-center relative z-5 gap-2 bg-gray-800 text-white px-6 sm:px-16 py-3 rounded-lg font-semibold">
                     <BsPlusCircle size={20} />
                     Find a place
                 </button></Link>
@@ -63,9 +63,9 @@ function HomePage() {
 
       {/** view popular cities */}
 
-       <div className="py-10 px-12">
+       <div className="p-5 sm:py-10 sm:px-12">
             <h2 className="text-4xl font-light mb-6">View rooms in popular cities</h2>
-            <div className="grid grid-cols-5 gap-4">
+            <div className="grid sm:grid-cols-5 grid-cols-2 gap-4">
                 {cities.slice(0,10).map((city, index) => (
                     <div
                         onClick={()=>handleClick(city.city)}
@@ -95,27 +95,27 @@ function HomePage() {
             {/* Logos */}
             <div className="flex justify-between items-center flex-wrap mb-6">
                 {/* Logo placeholders */}
-                <div className="text-gray-400 text-xl font-bold">
+                <div className="text-gray-400 m-1 text-xl font-bold">
                 <span className="block p-4 bg-gray-200 rounded-lg flex items-center justify-center">
                     The Hindu
                 </span>
                 </div>
-                <div className="text-gray-400 text-xl font-bold">
+                <div className="text-gray-400 m-1 text-xl font-bold">
                 <span className="block p-4 bg-gray-200 rounded-lg flex items-center justify-center">
                     Times of India
                 </span>
                 </div>
-                <div className="text-gray-400 text-xl font-bold">
+                <div className="text-gray-400 m-1 text-xl font-bold">
                 <span className="block p-4 bg-gray-200 rounded-lg flex items-center justify-center">
                     Hindustan Times
                 </span>
                 </div>
-                <div className="text-gray-400 text-xl font-bold">
+                <div className="text-gray-400 m-1 text-xl font-bold">
                 <span className="block p-4 bg-gray-200 rounded-lg flex items-center justify-center">
                     Indian Express
                 </span>
                 </div>
-                <div className="text-gray-400 text-xl font-bold">
+                <div className="text-gray-400 m-1 text-xl font-bold">
                 <span className="block  p-4 bg-gray-200 rounded-lg flex items-center justify-center">
                     Dainik Bhaskar
                 </span>
