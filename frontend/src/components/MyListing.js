bee94ee65e715c9398bf974feb2dbb82d44f4983
vijaysingh60 <121249393+vijaysingh60@users.noreply.github.com>
2025-02-14 09:11:46 +0530

    Update MyListing.js

--- frontend/src/components/MyListing.js
+++ frontend/src/components/MyListing.js
@@ -32,18 +32,18 @@ import url from "./auth/backendUrl";
         },[])
         const imgurl = userInfo && 'http://localhost:3001/' + userInfo.profilePicture;
 return (
-    <div className="p-6 bg-gray-100 min-h-screen pt-24 flex flex-col justify-center items-center">
+    <div className="sm:p-6 p-3 bg-gray-100 min-h-screen pt-24 flex flex-col justify-center items-center">
         {/* User Profile Section */}
-        <div className="flex items-center space-x-4 bg-white p-4 rounded shadow w-2/3 justify-between">
+        <div className="flex items-center space-x-4 bg-white p-4 rounded shadow sm:w-2/3 justify-between">
             <div className="flex items-center">
-                <div className=" h-16 w-16 rounded-full flex items-center overflow-hidden bg-gray-200 mx-2  justify-center text-gray-400">
+                <div className="h-8 w-8 sm:h-16 sm:w-16 rounded-full flex items-center overflow-hidden bg-gray-200 mx-2  justify-center text-gray-400">
                     {userInfo && userInfo.profilePicture ?                            
                         <img src={imgurl} className="w-full h-full" alt='profile'/> :
                         <FaUser className="text-gray-500 mt-1 text-4xl" />
                     }
                 </div>
-            <h2 className="text-xl font-medium capitalize">{userInfo && userInfo.name},
-                <span className="text-md text-gray-700">{userInfo && " "+userInfo.age || " age"}</span>
+            <h2 className="sm:text-xl text-md font-medium capitalize">{userInfo && userInfo.name},
+                <span className="sm:text-md text-sm text-gray-700">{userInfo && " "+userInfo.age || " age"}</span>
             </h2>
             <span className="text-yellow-600 font-bold mx-2">{userInfo && userInfo.gender === 'Male'?<IoMdMale />:<IoMdFemale />}</span>
             </div>
@@ -53,10 +53,10 @@ return (
         </div>
 
         {/* Listings Section */}
-        <div className="mt-6 flex w-2/3 flex-wrap">
+        <div className="mt-6 flex sm:w-2/3 flex-wrap">
             {/* Listing  */}
             {roomArr.map((value,idx)=>(
-                <div key={idx} className="w-1/2 p-2">
+                <div key={idx} className="sm:w-1/2 p-2">
                 <Room props={value} /></div>
             ))}
 
